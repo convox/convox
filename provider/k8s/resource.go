507fb8d7333e607e30ef439550a85db1ea517272
David Dollar <david@dollar.io>
2019-08-27 09:13:38 -0400

    implement resource get and list inside k8s

--- provider/k8s/resource.go
+++ provider/k8s/resource.go
@@ -4,14 +4,58 @@ import (
 	"fmt"
 
 	"github.com/convox/convox/pkg/structs"
+	am "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func (p *Provider) ResourceGet(app, name string) (*structs.Resource, error) {
-	return nil, fmt.Errorf("unimplemented")
+	d, err := p.Cluster.AppsV1().Deployments(p.AppNamespace(app)).Get(fmt.Sprintf("resource-%s", name), am.GetOptions{})
+	if err != nil {
+		return nil, err
+	}
+
+	cm, err := p.Cluster.CoreV1().ConfigMaps(p.AppNamespace(app)).Get(fmt.Sprintf("resource-%s", name), am.GetOptions{})
+	if err != nil {
+		return nil, err
+	}
+
+	status := "running"
+
+	if d.Status.ReadyReplicas < 1 {
+		status = "pending"
+	}
+
+	r := &structs.Resource{
+		Name:   name,
+		Status: status,
+		Type:   d.ObjectMeta.Labels["kind"],
+		Url:    cm.Data["URL"],
+	}
+
+	return r, nil
 }
 
 func (p *Provider) ResourceList(app string) (structs.Resources, error) {
-	return nil, fmt.Errorf("unimplemented")
+	lopts := am.ListOptions{
+		LabelSelector: fmt.Sprintf("app=%s,type=resource", app),
+	}
+
+	ds, err := p.Cluster.AppsV1().Deployments(p.AppNamespace(app)).List(lopts)
+	if err != nil {
+		return nil, err
+	}
+
+	rs := structs.Resources{}
+
+	for _, d := range ds.Items {
+		r, err := p.ResourceGet(app, d.ObjectMeta.Labels["resource"])
+		if err != nil {
+			return nil, err
+		}
+
+		rs = append(rs, *r)
+	}
+
+	return rs, nil
 }
 
 func (p *Provider) SystemResourceCreate(kind string, opts structs.ResourceCreateOptions) (*structs.Resource, error) {
