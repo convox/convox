7cc4b0cfc72be2fd56993599b4e4ab050dd9345f
David Dollar <david@dollar.io>
2019-11-06 11:05:53 -0500

    use random suffix for redis cluster name

--- terraform/router/do/redis.tf
+++ terraform/router/do/redis.tf
@@ -1,5 +1,11 @@
+resource "random_string" "suffix" {
+  length  = 12
+  special = false
+  upper   = false
+}
+
 resource "digitalocean_database_cluster" "cache" {
-  name       = "${var.name}-router"
+  name       = "${var.name}-router-${random_string.suffix.result}"
   engine     = "redis"
   size       = "db-s-1vcpu-1gb"
   region     = var.region
