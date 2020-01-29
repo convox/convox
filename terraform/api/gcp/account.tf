c099d40fcba32958b447d5c24c6aac4f72698e69
David Dollar <david@dollar.io>
2020-01-29 10:16:25 -0500

    update logging reader for new kubernetes format

--- terraform/api/gcp/account.tf
+++ terraform/api/gcp/account.tf
@@ -6,13 +6,8 @@ resource "google_service_account_key" "api" {
   service_account_id = google_service_account.api.name
 }
 
-resource "google_project_iam_member" "api-logging-viewer" {
-  role   = "roles/logging.viewer"
-  member = "serviceAccount:${google_service_account.api.email}"
-}
-
-resource "google_project_iam_member" "api-logging-writer" {
-  role   = "roles/logging.logWriter"
+resource "google_project_iam_member" "api-logging-admin" {
+  role   = "roles/logging.admin"
   member = "serviceAccount:${google_service_account.api.email}"
 }
 
