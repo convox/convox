d54ce332a7f23cd4e3589935f2e8c34a241838e6
David Dollar <david@convox.com>
2020-02-13 08:27:15 -0500

    cli: use github releases to update (#115)
    * cli: use github releases to update
    
    * fix import namespace

--- pkg/cli/version.go
+++ pkg/cli/version.go
@@ -1,22 +1,22 @@
 package cli
 
 import (
-	rck "github.com/convox/convox/pkg/rack"
+	"github.com/convox/convox/pkg/rack"
 	"github.com/convox/convox/sdk"
 	"github.com/convox/stdcli"
 )
 
 func init() {
-	register("version", "display version information", Version, stdcli.CommandOptions{
+	registerWithoutProvider("version", "display version information", Version, stdcli.CommandOptions{
 		Flags:    []stdcli.Flag{flagRack},
 		Validate: stdcli.Args(0),
 	})
 }
 
-func Version(rack sdk.Interface, c *stdcli.Context) error {
+func Version(_ sdk.Interface, c *stdcli.Context) error {
 	c.Writef("client: <info>%s</info>\n", c.Version())
 
-	r, err := rck.Current(c)
+	r, err := rack.Current(c)
 	if err != nil {
 		c.Writef("server: <info>none</info>\n")
 		return nil
