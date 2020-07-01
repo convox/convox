74b7cf64173d7fca3755323b2a169242259a0ddc
Ed Fricker <888909+beastawakens@users.noreply.github.com>
2020-07-01 20:55:38 +0100

    k8s: return ports in process list (#257)
    * k8s: Returning pod IP as host, and handling port 0 more elegantly
    
    * k8s: formatting
    
    * k8s: fix Deepsource issue CRT-A0017
    
    * k8s: argument order is important!
    
    * gofmt
    
    Co-authored-by: David Dollar <david@dollar.io>

--- provider/k8s/process_test.go
+++ provider/k8s/process_test.go
@@ -15,6 +15,10 @@ import (
 )
 
 func processCreate(c kubernetes.Interface, ns, name, labels string) error {
+	return processCreatePorts(c, ns, name, labels, "127.0.0.1", 123, 4567)
+}
+
+func processCreatePorts(c kubernetes.Interface, ns, name, labels, podIp string, hostPort, containerPort int32) error {
 	om := am.ObjectMeta{
 		Labels: map[string]string{},
 		Name:   name,
@@ -27,14 +31,17 @@ func processCreate(c kubernetes.Interface, ns, name, labels string) error {
 
 	p := &ac.Pod{
 		ObjectMeta: om,
+		Status: ac.PodStatus{
+			PodIP: podIp,
+		},
 		Spec: ac.PodSpec{
 			Containers: []ac.Container{
 				{
 					Name: "main",
 					Ports: []ac.ContainerPort{
 						{
-							ContainerPort: 4567,
-							HostPort:      123,
+							ContainerPort: containerPort,
+							HostPort:      hostPort,
 						},
 					},
 				},
@@ -55,21 +62,31 @@ func TestProcessList(t *testing.T) {
 
 		require.NoError(t, appCreate(kk, "rack1", "app1"))
 
-		require.NoError(t, processCreate(kk, "rack1-app1", "process1", "system=convox,rack=rack1,app=app1,service=service1,type=service"))
-		require.NoError(t, processCreate(kk, "rack1-app1", "process2", "system=convox,rack=rack1,app=app1,service=service2,type=process"))
+		require.NoError(t, processCreatePorts(kk, "rack1-app1", "process1", "system=convox,rack=rack1,app=app1,service=service1,type=service", "1.2.3.4", 111, 2222))
+		require.NoError(t, processCreatePorts(kk, "rack1-app1", "process2", "system=convox,rack=rack1,app=app1,service=service2,type=process", "5.6.7.8", 333, 4444))
+		require.NoError(t, processCreatePorts(kk, "rack1-app1", "process3", "system=convox,rack=rack1,app=app1,service=service2,type=process", "9.10.11.12", 0, 5555))
 
 		pss, err := p.ProcessList("app1", structs.ProcessListOptions{})
 		require.NoError(t, err)
-		require.Len(t, pss, 2)
+		require.Len(t, pss, 3)
 
 		require.Equal(t, "process1", pss[0].Id)
 		require.Len(t, pss[0].Ports, 1)
-		require.Equal(t, "123:4567", pss[0].Ports[0])
+		require.Equal(t, "111:2222", pss[0].Ports[0])
 		require.Equal(t, "service1", pss[0].Name)
+		require.Equal(t, "1.2.3.4", pss[0].Host)
 
 		require.Equal(t, "process2", pss[1].Id)
 		require.Len(t, pss[1].Ports, 1)
-		require.Equal(t, "123:4567", pss[1].Ports[0])
+		require.Equal(t, "333:4444", pss[1].Ports[0])
 		require.Equal(t, "service2", pss[1].Name)
+		require.Equal(t, "5.6.7.8", pss[1].Host)
+
+		require.Equal(t, "process3", pss[2].Id)
+		require.Len(t, pss[2].Ports, 1)
+		require.Equal(t, "5555", pss[2].Ports[0])
+		require.Equal(t, "service2", pss[2].Name)
+		require.Equal(t, "9.10.11.12", pss[2].Host)
+
 	})
 }
