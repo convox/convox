b4c811c33a4d7d9e19fea64c01697a372e334469
David Dollar <david@convox.com>
2020-07-02 11:35:21 -0400

    k8s: restructure process test (#258)

--- provider/k8s/process_test.go
+++ provider/k8s/process_test.go
@@ -14,6 +14,41 @@ import (
 	"k8s.io/client-go/kubernetes/fake"
 )
 
+func TestProcessList(t *testing.T) {
+	testProvider(t, func(p *k8s.Provider) {
+		kk := p.Cluster.(*fake.Clientset)
+
+		require.NoError(t, appCreate(kk, "rack1", "app1"))
+
+		require.NoError(t, processCreatePorts(kk, "rack1-app1", "process1", "system=convox,rack=rack1,app=app1,service=service1,type=service", "1.2.3.4", 111, 2222))
+		require.NoError(t, processCreatePorts(kk, "rack1-app1", "process2", "system=convox,rack=rack1,app=app1,service=service2,type=process", "5.6.7.8", 333, 4444))
+		require.NoError(t, processCreatePorts(kk, "rack1-app1", "process3", "system=convox,rack=rack1,app=app1,service=service2,type=process", "9.10.11.12", 0, 5555))
+
+		pss, err := p.ProcessList("app1", structs.ProcessListOptions{})
+		require.NoError(t, err)
+		require.Len(t, pss, 3)
+
+		require.Equal(t, "process1", pss[0].Id)
+		require.Len(t, pss[0].Ports, 1)
+		require.Equal(t, "111:2222", pss[0].Ports[0])
+		require.Equal(t, "service1", pss[0].Name)
+		require.Equal(t, "1.2.3.4", pss[0].Host)
+
+		require.Equal(t, "process2", pss[1].Id)
+		require.Len(t, pss[1].Ports, 1)
+		require.Equal(t, "333:4444", pss[1].Ports[0])
+		require.Equal(t, "service2", pss[1].Name)
+		require.Equal(t, "5.6.7.8", pss[1].Host)
+
+		require.Equal(t, "process3", pss[2].Id)
+		require.Len(t, pss[2].Ports, 1)
+		require.Equal(t, "5555", pss[2].Ports[0])
+		require.Equal(t, "service2", pss[2].Name)
+		require.Equal(t, "9.10.11.12", pss[2].Host)
+
+	})
+}
+
 func processCreate(c kubernetes.Interface, ns, name, labels string) error {
 	return processCreatePorts(c, ns, name, labels, "127.0.0.1", 123, 4567)
 }
@@ -54,39 +89,4 @@ func processCreatePorts(c kubernetes.Interface, ns, name, labels, podIp string, 
 	}
 
 	return nil
-}
-
-func TestProcessList(t *testing.T) {
-	testProvider(t, func(p *k8s.Provider) {
-		kk := p.Cluster.(*fake.Clientset)
-
-		require.NoError(t, appCreate(kk, "rack1", "app1"))
-
-		require.NoError(t, processCreatePorts(kk, "rack1-app1", "process1", "system=convox,rack=rack1,app=app1,service=service1,type=service", "1.2.3.4", 111, 2222))
-		require.NoError(t, processCreatePorts(kk, "rack1-app1", "process2", "system=convox,rack=rack1,app=app1,service=service2,type=process", "5.6.7.8", 333, 4444))
-		require.NoError(t, processCreatePorts(kk, "rack1-app1", "process3", "system=convox,rack=rack1,app=app1,service=service2,type=process", "9.10.11.12", 0, 5555))
-
-		pss, err := p.ProcessList("app1", structs.ProcessListOptions{})
-		require.NoError(t, err)
-		require.Len(t, pss, 3)
-
-		require.Equal(t, "process1", pss[0].Id)
-		require.Len(t, pss[0].Ports, 1)
-		require.Equal(t, "111:2222", pss[0].Ports[0])
-		require.Equal(t, "service1", pss[0].Name)
-		require.Equal(t, "1.2.3.4", pss[0].Host)
-
-		require.Equal(t, "process2", pss[1].Id)
-		require.Len(t, pss[1].Ports, 1)
-		require.Equal(t, "333:4444", pss[1].Ports[0])
-		require.Equal(t, "service2", pss[1].Name)
-		require.Equal(t, "5.6.7.8", pss[1].Host)
-
-		require.Equal(t, "process3", pss[2].Id)
-		require.Len(t, pss[2].Ports, 1)
-		require.Equal(t, "5555", pss[2].Ports[0])
-		require.Equal(t, "service2", pss[2].Name)
-		require.Equal(t, "9.10.11.12", pss[2].Host)
-
-	})
 }
