769d30ce9024867fbcb6fc20bf22af22e54b293a
David Dollar <david@convox.com>
2020-07-20 16:38:34 -0400

    ci: specify install region for each provider (#282)

--- ci/install.sh
+++ ci/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -ex -o pipefail
 
-convox rack install ${PROVIDER} ${RACK_NAME} -v ${VERSION}
+convox rack install ${PROVIDER} ${RACK_NAME} -v ${VERSION} region=${REGION}
--- ci/secrets.sh
+++ ci/secrets.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
 export_secret() {
-	echo "::set-env name=${2:-$1}::$(echo $SECRETS | jq -r ".${1}")"
+  echo "::set-env name=${2:-$1}::$(echo $SECRETS | jq -r ".${1}")"
 }
 
 case "$PROVIDER" in
 aws)
   export_secret AWS_ACCESS_KEY_ID
+  export_secret AWS_REGION REGION
   export_secret AWS_SECRET_ACCESS_KEY
   ;;
 azure)
   export_secret ARM_CLIENT_ID
   export_secret ARM_CLIENT_SECRET
+  export_secret ARM_REGION REGION
   export_secret ARM_SUBSCRIPTION_ID
   export_secret ARM_TENANT_ID
   ;;
 do)
   export_secret DIGITALOCEAN_ACCESS_ID
-  export_secret DIGITALOCEAN_REGION TF_VAR_region
+  export_secret DIGITALOCEAN_REGION REGION
   export_secret DIGITALOCEAN_SECRET_KEY
   export_secret DIGITALOCEAN_TOKEN
   ;;
@@ -25,5 +27,6 @@ gcp)
   export_secret GOOGLE_CREDENTIALS
   export_secret GOOGLE_PROJECT
   export_secret GOOGLE_REGION
+  export_secret GOOGLE_REGION REGION
   ;;
 esac
