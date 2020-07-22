b65866a04087c5ba6f4b89fc9567268d80d00890
David Dollar <david@convox.com>
2020-07-22 09:21:59 -0400

    k8s: propagate release descriptions (#284)
    * k8s: propagate release descriptions
    
    * fix tests

--- provider/k8s/release_test.go
+++ provider/k8s/release_test.go
@@ -8,6 +8,7 @@ import (
 	"testing"
 
 	"github.com/convox/convox/pkg/atom"
+	"github.com/convox/convox/pkg/options"
 	"github.com/convox/convox/pkg/structs"
 	"github.com/convox/convox/provider/k8s"
 	ca "github.com/convox/convox/provider/k8s/pkg/apis/convox/v1"
@@ -19,6 +20,60 @@ import (
 	am "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes/fake"
 )
+
+func TestReleaseCreate(t *testing.T) {
+	testProvider(t, func(p *k8s.Provider) {
+		aa := p.Atom.(*atom.MockInterface)
+		kc := p.Convox.(*cvfake.Clientset)
+		kk := p.Cluster.(*fake.Clientset)
+
+		aa.On("Status", "rack1-app1", "app").Return("Running", "release1", nil)
+
+		require.NoError(t, appCreate(kk, "rack1", "app1"))
+		require.NoError(t, buildCreate(kc, "rack1-app1", "build1", "basic"))
+		require.NoError(t, buildCreate(kc, "rack1-app1", "build2", "basic"))
+
+		opts1 := structs.ReleaseCreateOptions{
+			Build: options.String("build1"),
+		}
+
+		r1, err := p.ReleaseCreate("app1", opts1)
+		require.NoError(t, err)
+		require.NotNil(t, r1)
+		require.Equal(t, "app1", r1.App)
+		require.Equal(t, "build1", r1.Build)
+		require.Equal(t, "foo", r1.Description)
+		require.Equal(t, "", r1.Env)
+		require.Equal(t, "services:\n  web:\n    build: .\n    port: 5000\n", r1.Manifest)
+
+		opts2 := structs.ReleaseCreateOptions{
+			Env: options.String("FOO=bar"),
+		}
+
+		r2, err := p.ReleaseCreate("app1", opts2)
+		require.NoError(t, err)
+		require.NotNil(t, r2)
+		require.Equal(t, "app1", r2.App)
+		require.Equal(t, "build1", r2.Build)
+		require.Equal(t, "env add:FOO", r2.Description)
+		require.Equal(t, "FOO=bar", r2.Env)
+		require.Equal(t, "services:\n  web:\n    build: .\n    port: 5000\n", r2.Manifest)
+
+		opts3 := structs.ReleaseCreateOptions{
+			Build:       options.String("build2"),
+			Description: options.String("desc3"),
+		}
+
+		r3, err := p.ReleaseCreate("app1", opts3)
+		require.NoError(t, err)
+		require.NotNil(t, r3)
+		require.Equal(t, "app1", r3.App)
+		require.Equal(t, "build2", r3.Build)
+		require.Equal(t, "desc3", r3.Description)
+		require.Equal(t, "FOO=bar", r3.Env)
+		require.Equal(t, "services:\n  web:\n    build: .\n    port: 5000\n", r3.Manifest)
+	})
+}
 
 func TestReleasePromote(t *testing.T) {
 	testProvider(t, func(p *k8s.Provider) {
@@ -78,10 +133,11 @@ func releaseFixture(name string) (*ca.ReleaseSpec, error) {
 	}
 
 	var fixture struct {
-		Build    string
-		Created  string
-		Env      map[string]string
-		Manifest interface{}
+		Build       string
+		Created     string
+		Description string
+		Env         map[string]string
+		Manifest    interface{}
 	}
 
 	if err := yaml.Unmarshal(data, &fixture); err != nil {
@@ -102,10 +158,11 @@ func releaseFixture(name string) (*ca.ReleaseSpec, error) {
 	}
 
 	s := &ca.ReleaseSpec{
-		Build:    fixture.Build,
-		Created:  fixture.Created,
-		Env:      strings.Join(ep, "\n"),
-		Manifest: string(mdata),
+		Build:       fixture.Build,
+		Created:     fixture.Created,
+		Description: fixture.Description,
+		Env:         strings.Join(ep, "\n"),
+		Manifest:    string(mdata),
 	}
 
 	return s, nil
