0ae51b87473466cf453479307b696266172476bb
David Dollar <david@dollar.io>
2020-08-19 13:33:39 -0400

    redis: remove unused terraform modules

--- terraform/redis/azure/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "name" {
-  type = string
-}
-
-variable "resource_group" {
-  type = string
-}
--- terraform/redis/gcp/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "name" {
-  type = string
-}
-
-variable "network" {
-  type = string
-}
