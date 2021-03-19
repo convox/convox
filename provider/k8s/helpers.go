94c3b6c70c150dbbdb7790e85cd2682c7377cd73
Ed Fricker <888909+beastawakens@users.noreply.github.com>
2021-03-19 16:56:04 +0000

    Mounting /sys/kernel/debug/ for datadog (#330)

--- provider/k8s/helpers.go
+++ provider/k8s/helpers.go
@@ -290,6 +290,8 @@ func systemVolume(v string) bool {
 		return true
 	case "/sys/fs/cgroup/":
 		return true
+	case "/sys/kernel/debug/":
+		return true
 	case "/var/log/audit/":
 		return true
 	case "/var/run/":
