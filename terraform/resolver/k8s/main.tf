ae3299b31383bc9758ed96db10dc1dbc702a797b
David Dollar <david@dollar.io>
2021-05-05 10:17:24 -0400

    terraform: updates for arm64

--- terraform/resolver/k8s/main.tf
+++ terraform/resolver/k8s/main.tf
@@ -161,7 +161,7 @@ resource "kubernetes_deployment" "resolver" {
           }
 
           resources {
-            requests {
+            requests = {
               cpu    = "64m"
               memory = "64Mi"
             }
