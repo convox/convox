45a84c20de43b136aa0288a746a7b268059feec3
David Dollar <david@dollar.io>
2021-07-19 18:25:46 -0400

    metal certificates should be self-signed

--- provider/metal/ingress.go
+++ provider/metal/ingress.go
@@ -2,7 +2,7 @@ package metal
 
 func (p *Provider) IngressAnnotations(app string) (map[string]string, error) {
 	ans := map[string]string{
-		"cert-manager.io/cluster-issuer": "letsencrypt",
+		"cert-manager.io/cluster-issuer": "self-signed",
 	}
 
 	return ans, nil
