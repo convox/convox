560c478dcd60ffcb8f2bbfec541b335ec94c448f
David Dollar <david@dollar.io>
2021-07-19 12:04:23 -0400

    workaround for terraform resolution inside same module

--- terraform/cluster/aws/main.tf
+++ terraform/cluster/aws/main.tf
@@ -4,6 +4,13 @@ provider "kubernetes" {
   token                  = data.aws_eks_cluster_auth.cluster.token
 }
 
+provider "kubernetes" {
+  alias                  = "direct"
+  cluster_ca_certificate = base64decode(aws_eks_cluster.cluster.certificate_authority.0.data)
+  host                   = aws_eks_cluster.cluster.endpoint
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
 locals {
   oidc_sub           = "${replace(aws_iam_openid_connect_provider.cluster.url, "https://", "")}:sub"
   gpu_type           = substr(var.node_type, 0, 1) == "g" || substr(var.node_type, 0, 1) == "p"
