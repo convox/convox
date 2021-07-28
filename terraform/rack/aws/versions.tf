5bfbd6417c621f488e5f4cb14536daec53aed273
Ed Fricker <edafricker@gmail.com>
2021-07-28 15:19:48 +0100

    consistency

--- terraform/rack/aws/versions.tf
+++ terraform/rack/aws/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 2.49"
+      version = "~> 3.31"
     }
     external = {
       source  = "hashicorp/external"
