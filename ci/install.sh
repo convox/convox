8e680e0eb482a1de7e4deebd3c36dbf0fa0eca5c
Heron Rossi <rossiheron@gmail.com>
2021-10-28 11:59:05 -0300

    setup ci test for arm64 attempt 1

--- ci/install.sh
+++ ci/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -ex -o pipefail
 
-convox rack install ${PROVIDER} ${RACK_NAME} -v ${VERSION} region=${REGION}
+convox rack install ${PROVIDER} ${RACK_NAME} -v ${VERSION} ${RACK_PARAMS}
