c42b3c0f575fc81f3d72123a68a80923862cf029
Heron Rossi <rossiheron@gmail.com>
2021-10-28 18:26:25 -0300

    ci: add region param back when creating racks

--- ci/install.sh
+++ ci/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -ex -o pipefail
 
-convox rack install ${PROVIDER} ${RACK_NAME} -v ${VERSION} ${RACK_PARAMS}
+convox rack install ${PROVIDER} ${RACK_NAME} -v ${VERSION} region=${REGION} ${RACK_PARAMS}
