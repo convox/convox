36568cc7ffc5cc0faafd479839a0e7146b436e72
Heron Rossi <rossiheron@gmail.com>
2021-11-02 18:28:08 -0300

    pin aws provider version

--- terraform/cluster/aws/versions.tf
+++ terraform/cluster/aws/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.31"
+      version = "3.33"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
