f28a2f5a9bec416fa03cec7434de2e84a71e8500
Heron Rossi <rossiheron@gmail.com>
2021-11-02 10:02:33 -0300

    downgrade aws provider

--- terraform/router/aws/versions.tf
+++ terraform/router/aws/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.31"
+      version = "~> 2.49"
     }
     http = {
       source  = "hashicorp/http"
