635094b5b42cdcd9b819093d64aff3db7ecd3346
Ed Fricker <edafricker@gmail.com>
2021-11-02 15:12:44 +0000

    Establish local architecture for arm support

--- terraform/system/local/main.tf
+++ terraform/system/local/main.tf
@@ -3,8 +3,9 @@ data "http" "releases" {
 }
 
 locals {
+  arm_type = module.platform.arch == "arm64"
   current = jsondecode(data.http.releases.body).tag_name
-  release = coalesce(var.release, local.current)
+  release = local.arm_type ? format("%s-%s", coalesce(var.release, local.current), "arm64") : coalesce(var.release, local.current)
 }
 
 provider "kubernetes" {
