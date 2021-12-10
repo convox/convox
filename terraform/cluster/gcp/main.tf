2b31bfb58f350718a4738f70ec3ddf28978fa77a
Heron Rossi <rossiheron@gmail.com>
2021-12-10 15:44:27 -0300

    remove redundant provider block

--- terraform/cluster/gcp/main.tf
+++ terraform/cluster/gcp/main.tf
@@ -94,15 +94,13 @@ resource "local_file" "kubeconfig" {
   }
 }
 
-data "google_client_config" "provider" {}
-
 provider "kubernetes" {
   alias = "direct"
 
   load_config_file = false
 
   host                   = "https://${google_container_cluster.rack.endpoint}"
-  token                  = data.google_client_config.provider.access_token
+  token                  = data.google_client_config.current.access_token
   cluster_ca_certificate = base64decode(google_container_cluster.rack.master_auth.0.cluster_ca_certificate)
 }
 
