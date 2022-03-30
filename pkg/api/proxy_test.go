b84910269370a22aa9d7bcace3f7cb477ddba966
Robert Sköld <robert@publicclass.se>
2022-03-30 10:41:15 +0200

    Update proxy test with more sizes

--- pkg/api/proxy_test.go
+++ pkg/api/proxy_test.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
-	"strings"
+	"math/rand"
 	"testing"
 
 	"github.com/convox/convox/pkg/structs"
@@ -14,28 +14,44 @@ import (
 )
 
 func TestProxy(t *testing.T) {
-	testServer(t, func(c *stdsdk.Client, p *structs.MockProvider) {
-		var s string
-		for i := 0; len(s) < 1024; i++ {
-			s += strconv.Itoa(i)
-		}
+	tests := map[string]struct {
+		size int64
+	}{
+		"0b":    {size: 0},
+		"1b":    {size: 1},
+		"128b":  {size: 128},
+		"500b":  {size: 500},
+		"1kb":   {size: 1000},
+		"1kib":  {size: 1024},
+		"2kib":  {size: 1024 * 2},
+		"8kib":  {size: 1024 * 8},
+		"16kib": {size: 1024 * 16},
+		"1mib":  {size: 1024 * 1024},
+		"8mib":  {size: 1024 * 1024 * 8},
+	}
 
-		ro := stdsdk.RequestOptions{
-			Body: bytes.NewBufferString(s),
-		}
-		p.On("Proxy", "host", 5000, mock.Anything, structs.ProxyOptions{}).Return(nil).Run(func(args mock.Arguments) {
-			rw := args.Get(2).(io.ReadWriter)
-			rw.Write([]byte(s))
-			data, err := ioutil.ReadAll(rw)
-			require.NoError(t, err)
-			require.Equal(t, s, string(data))
+	for desc, tt := range tests {
+		t.Run(desc, func(t *testing.T) {
+			testServer(t, func(c *stdsdk.Client, p *structs.MockProvider) {
+				ro := stdsdk.RequestOptions{
+					Body: io.LimitReader(rand.New(rand.NewSource(1028890720402726901)), tt.size),
+				}
+
+				p.On("Proxy", "host", 5000, mock.Anything, structs.ProxyOptions{}).Return(nil).Run(func(args mock.Arguments) {
+					rw := args.Get(2).(io.ReadWriter)
+					// "echo"
+					n, err := io.Copy(rw, rw)
+					require.NoError(t, err)
+					require.Equal(t, tt.size, int64(n))
+				})
+				r, err := c.Websocket("/proxy/host/5000", ro)
+				require.NoError(t, err)
+				data, err := ioutil.ReadAll(r)
+				require.NoError(t, err)
+				require.Len(t, data, int(tt.size))
+			})
 		})
-		r, err := c.Websocket("/proxy/host/5000", ro)
-		require.NoError(t, err)
-		data, err := ioutil.ReadAll(r)
-		require.NoError(t, err)
-		require.Equal(t, s, string(data))
-	})
+	}
 }
 
 func TestProxyError(t *testing.T) {
