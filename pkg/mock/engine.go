dcda49391515ee92fe04b539ce8cc0154f7cdc3f
yuriadams <yuriadams@gmail.com>
2022-09-15 14:51:27 -0300

    fix deepsource code lint: Unused method receiver

--- pkg/mock/engine.go
+++ pkg/mock/engine.go
@@ -11,62 +11,62 @@ import (
 type TestEngine struct {
 }
 
-func (tr *TestEngine) AppIdles(app string) (bool, error) {
+func (*TestEngine) AppIdles(app string) (bool, error) {
 	return false, nil
 }
 
-func (te *TestEngine) AppParameters() map[string]string {
+func (*TestEngine) AppParameters() map[string]string {
 	return map[string]string{"Test": "foo"}
 }
 
-func (te *TestEngine) Heartbeat() (map[string]interface{}, error) {
+func (*TestEngine) Heartbeat() (map[string]interface{}, error) {
 	return map[string]interface{}{"foo": "bar"}, nil
 }
 
-func (te *TestEngine) IngressAnnotations(app string) (map[string]string, error) {
+func (*TestEngine) IngressAnnotations(app string) (map[string]string, error) {
 	return map[string]string{"ann1": "val1"}, nil
 }
 
-func (te *TestEngine) IngressClass() string {
+func (*TestEngine) IngressClass() string {
 	return ""
 }
 
-func (te *TestEngine) Log(app, stream string, ts time.Time, message string) error {
+func (*TestEngine) Log(app, stream string, ts time.Time, message string) error {
 	return nil
 }
 
-func (te *TestEngine) ManifestValidate(m *manifest.Manifest) error {
+func (*TestEngine) ManifestValidate(m *manifest.Manifest) error {
 	return nil
 }
 
-func (te *TestEngine) RegistryAuth(host, username, password string) (string, string, error) {
+func (*TestEngine) RegistryAuth(host, username, password string) (string, string, error) {
 	return username, password, nil
 }
 
-func (te *TestEngine) RepositoryAuth(app string) (string, string, error) {
+func (*TestEngine) RepositoryAuth(app string) (string, string, error) {
 	return "un1", "pw1", nil
 }
 
-func (te *TestEngine) RepositoryHost(app string) (string, bool, error) {
+func (*TestEngine) RepositoryHost(app string) (string, bool, error) {
 	return "repo1", true, nil
 }
 
-func (te *TestEngine) RepositoryPrefix() string {
+func (*TestEngine) RepositoryPrefix() string {
 	return ""
 }
 
-func (te *TestEngine) ResolverHost() (string, error) {
+func (*TestEngine) ResolverHost() (string, error) {
 	return "", errors.WithStack(fmt.Errorf("no resolver"))
 }
 
-func (te *TestEngine) ServiceHost(app string, s manifest.Service) string {
+func (*TestEngine) ServiceHost(app string, s manifest.Service) string {
 	return "service.host"
 }
 
-func (te *TestEngine) SystemHost() string {
+func (*TestEngine) SystemHost() string {
 	return "system.host"
 }
 
-func (te *TestEngine) SystemStatus() (string, error) {
+func (*TestEngine) SystemStatus() (string, error) {
 	return "amazing", nil
 }
