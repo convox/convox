550921da74bcf9ffd590de480324da35c81a61d0
Yuri Adams Chaves Maia <yuriadams@gmail.com>
2022-09-16 15:24:16 -0300

    Add e2e tests for app intercomunication (#494)
    * test connection between 2 apps using internal dns
    
    * move test script inside test.sh and other ammends

--- ci/test.sh
+++ ci/test.sh
@@ -139,5 +139,25 @@ convox resources -a httpd | grep rediscache
 ps=$(convox api get /apps/httpd/processes | jq -r '.[]|select(.status=="running" and .name == "web")|.id' | head -n 1)
 convox exec $ps "/usr/scripts/redis_check.sh" -a httpd
 
+# app (httpd2)
+convox apps create httpd2
+convox apps
+convox apps | grep httpd2
+convox apps info httpd2 | grep running
+convox deploy -a httpd2
+endpoint=$(convox api get /apps/httpd2/services | jq -r '.[] | select(.name == "web") | .domain')
+fetch https://$endpoint | grep "It works"
+
+# test internal communication
+rackname=$(convox rack | grep 'Name' | xargs | cut -d ' ' -f2 )
+
+pshttpd=$(convox api get /apps/httpd/processes | jq -r '.[]|select(.status=="running" and .name == "web")|.id' | head -n 1)
+pshttpd2=$(convox api get /apps/httpd2/processes | jq -r '.[]|select(.status=="running" and .name == "web")|.id' | head -n 1)
+
+sleep 10
+convox exec $pshttpd "curl web.httpd2.$rackname.local" -a httpd | grep "It works"
+convox exec $pshttpd2 "curl web.httpd.$rackname.local" -a httpd2 | grep "It works"
+
 # cleanup
 convox apps delete httpd
+convox apps delete httpd2
