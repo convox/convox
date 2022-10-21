8e4156058f9cd11423d08dd67d522addfc63f84d
Heron Rossi <rossiheron@gmail.com>
2022-10-21 12:02:08 -0300

    Enable timers test again and change apiservice resource version

--- ci/test.sh
+++ ci/test.sh
@@ -132,8 +132,8 @@ echo "env var is correctly set"
 mv Dockerfile.original Dockerfile # replace the Dockerfile with the original copy
 
 # timers
-# sleep 30
-# $root/ci/test/timers.sh &
+sleep 30
+$root/ci/test/timers.sh &
 
 # postgres resource test
 $root/ci/test/resources_postgres.sh &
