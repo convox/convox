5f36086d59b2013d87e18e76ed8bbc6f1e87a6e3
Taynan Souza <taynan.tws@gmail.com>
2022-11-04 17:37:12 -0300

    Adding back null_resource.delay_token to DO token output

--- terraform/cluster/do/outputs.tf
+++ terraform/cluster/do/outputs.tf
@@ -15,5 +15,6 @@ output "name" {
 }
 
 output "token" {
-  value = digitalocean_kubernetes_cluster.rack.kube_config[0].token
+  depends_on = [null_resource.delay_token]
+  value      = digitalocean_kubernetes_cluster.rack.kube_config[0].token
 }
