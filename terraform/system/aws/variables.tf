2189ca7ff7725c34b4c8513304ff899f9bf3d3d1
lucastercas <lucas.macedo@dnx.solutions>
2023-02-09 01:44:54 -0300

    Change main subnets variable to string, add public subnets

--- terraform/system/aws/variables.tf
+++ terraform/system/aws/variables.tf
@@ -124,13 +124,13 @@ variable "vpc_cni_version" {
 }
 
 variable "private_subnets_ids" {
-  type    = list(string)
-  default = []
+  type    = string
+  default = ""
 }
 
 variable "public_subnets_ids" {
-  type    = list(string)
-  default = []
+  type    = string
+  default = ""
 }
 
 variable "whitelist" {
