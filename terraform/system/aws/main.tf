0d0ec91cb65b49e3f4560bd8dfa02c94910e9458
lucastercas <lucas.macedo@dnx.solutions>
2023-03-01 11:54:58 -0300

    Add compact

--- terraform/system/aws/main.tf
+++ terraform/system/aws/main.tf
@@ -69,8 +69,8 @@ module "cluster" {
   tags                     = local.tag_map
   vpc_cni_version          = var.vpc_cni_version
   vpc_id                   = var.vpc_id
-  private_subnets_ids      = split(",", var.private_subnets_ids)
-  public_subnets_ids       = split(",", var.public_subnets_ids)
+  private_subnets_ids      = compact(split(",", var.private_subnets_ids))
+  public_subnets_ids       = compact(split(",", var.public_subnets_ids))
 }
 
 resource "null_resource" "wait_for_cluster" {
