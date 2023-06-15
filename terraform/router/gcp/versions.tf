c2dec40e30b26009a440a838f37d4f515e652561
nightfury1204 <knightnahid@gmail.com>
2023-06-15 22:13:13 +0100

    Update do and gcp version

--- terraform/router/gcp/versions.tf
+++ terraform/router/gcp/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 4.3.0"
+      version = "~> 4.69.1"
     }
     http = {
       source  = "hashicorp/http"
--- terraform/system/gcp/project/versions.tf
+++ terraform/system/gcp/project/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 4.3.0"
+      version = "~> 4.69.1"
     }
   }
   required_version = ">= 0.12"
