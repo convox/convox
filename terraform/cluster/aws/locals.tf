bf84dc5b537251661db265f76932132674faca16
Lucas de Macedo <lucas.macedo@dnx.solutions>
2023-08-07 19:27:29 -0300

    Remove locals file

--- terraform/cluster/aws/locals.tf
+++
@@ -1,3 +0,0 @@
-locals {
-  validate_private_subnets_count = length(var.private_subnets_ids) == 0 ? true : (var.high_availability ? (length(var.private_subnets_ids) == 3 ? true : tobool("If high availability is enabled, there must be 3 private subnets on each AZ")) : true)
-}
