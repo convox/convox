982008193126ba4f7f2033116ef2a6b09d046d17
nightfury1204 <knightnahid@gmail.com>
2023-08-23 22:35:40 +0100

    Add dummy value to change nodegroup name

--- terraform/cluster/aws/main.tf
+++ terraform/cluster/aws/main.tf
@@ -67,6 +67,7 @@ resource "random_id" "node_group" {
   byte_length = 8
 
   keepers = {
+    dummy               = "1"
     node_capacity_type  = var.node_capacity_type
     node_disk           = var.node_disk
     node_type           = var.node_type
@@ -83,6 +84,7 @@ resource "random_id" "build_node_group" {
   byte_length = 8
 
   keepers = {
+    dummy               = "1"
     node_disk           = var.node_disk
     node_type           = var.build_node_type
     private_subnets_ids = join("-", local.private_subnets_ids)
