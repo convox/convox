aae2923a1f8dba899dee1a8e388ea22d07616767
nahid <knightnahid@gmail.com>
2023-10-31 14:06:09 +0000

    Downgrade aws vpc cni

--- terraform/system/aws/variables.tf
+++ terraform/system/aws/variables.tf
@@ -209,7 +209,7 @@ variable "vpc_id" {
 // https://docs.aws.amazon.com/eks/latest/userguide/managing-vpc-cni.html
 variable "vpc_cni_version" {
   type    = string
-  default = "v1.13.4-eksbuild.1"
+  default = "v1.13.3-eksbuild.1"
 }
 
 variable "whitelist" {
