cbbb7c262a8accc3c88849c70c4639e42dcb2074
numcys <naman.sharma00100@gmail.com>
2024-06-26 02:31:44 +0530

    adding output to rack install to console cmd

--- pkg/cli/rack.go
+++ pkg/cli/rack.go
@@ -257,6 +257,7 @@ func RackInstall(_ sdk.Interface, c *stdcli.Context) error {
 
 	if runtime != "" {
 		name = parts[0]+ "/" +parts[1]
+		c.Writef("Convox Rack installation initiated. Check the progress on the Console Racks page if desired.")
 	}
 
 	if err := rack.Install(c, slug, name, version, runtime, opts); err != nil {
