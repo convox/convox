d8ce2f09ebe6a2822afb5ac01e6e36c31455a7e7
Naman Sharma <84118525+numcys@users.noreply.github.com>
2024-07-24 22:01:38 +0530

    Adding support to use docker creds in convox build and services pod (#796)
    Co-authored-by: nightfury1204 <knightnahid@gmail.com>

--- terraform/api/aws/variables.tf
+++ terraform/api/aws/variables.tf
@@ -25,6 +25,16 @@ variable "docker_hub_authentication" {
   type = string
 }
 
+variable "docker_hub_username" {
+  type    = string
+  default = ""
+}
+
+variable "docker_hub_password" {
+  type    = string
+  default = ""
+}
+
 variable "domain" {
   type = string
 }
