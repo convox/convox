fc8b93fd5dbaae4eea9bd98fbbc77dbf494bfc02
numcys <naman.sharma00100@gmail.com>
2024-08-06 13:11:44 +0530

    adding script for adding params to kubelet config

--- terraform/cluster/aws/main.tf
+++ terraform/cluster/aws/main.tf
@@ -278,6 +278,42 @@ resource "aws_launch_template" "cluster" {
     }
   }
 
+  user_data = base64encode(<<-EOF
+  MIME-Version: 1.0
+  Content-Type: multipart/mixed; boundary="==MYBOUNDARY=="
+
+  --==MYBOUNDARY==
+  Content-Type: text/x-shellscript; charset="us-ascii"
+
+  #!/bin/bash
+  echo "Running custom user data script"
+  cat <<'SCRIPT' > /opt/modify_kubelet.sh
+  #!/bin/bash
+
+  # Install jq if not already installed
+  yum install -y jq
+
+  # Path to the kubelet configuration file
+  KUBELET_CONFIG_FILE="/etc/kubernetes/kubelet/kubelet-config.json"
+
+  # Modify the registryPullQPS and registryBurst parameters in the kubelet config file
+  jq '.registryPullQPS = ${var.registry_pull_qps} | .registryBurst = ${var.registry_burst}' $KUBELET_CONFIG_FILE > /tmp/kubelet-config.json
+  mv -f /tmp/kubelet-config.json $KUBELET_CONFIG_FILE
+
+  # Reload the systemd daemon to apply the changes
+  systemctl daemon-reload
+
+  # Restart the kubelet service to apply the new configuration
+  systemctl restart kubelet
+  SCRIPT
+
+  chmod +x /opt/modify_kubelet.sh
+  /opt/modify_kubelet.sh
+
+  --==MYBOUNDARY==--
+  EOF
+  )
+
   key_name = var.key_pair_name != "" ? var.key_pair_name : null
 }
 
