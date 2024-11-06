53afe9f7cea4ccc3b6099b341362437502959f7d
Md. Nure Alam Nahid <knightnahid@gmail.com>
2024-11-06 18:49:43 +0000

    Update u2f token login to use browser (#834)

--- pkg/console/auth.go
+++ pkg/console/auth.go
@@ -2,11 +2,19 @@ package console
 
 import (
 	"bytes"
+	"encoding/base64"
 	"fmt"
-	"io/ioutil"
+	"io"
+	"log"
+	"net"
 	"net/http"
+	"net/url"
+	"os"
 	"regexp"
+	"strings"
+	"time"
 
+	"github.com/convox/convox/pkg/common"
 	"github.com/convox/convox/pkg/token"
 	"github.com/convox/convox/sdk"
 	"github.com/convox/stdsdk"
@@ -44,21 +52,60 @@ func (c *Client) authenticator(cl *stdsdk.Client, res *http.Response) (http.Head
 		}
 		defer ares.Body.Close()
 
-		dres, err := ioutil.ReadAll(ares.Body)
+		dres, err := io.ReadAll(ares.Body)
 		if err != nil {
 			return nil, err
 		}
 
-		c.handler.Writef("Waiting for security token... ")
+		c.handler.Writef("Waiting for security token...")
 
-		data, err := token.Authenticate(dres)
-		if err != nil {
-			return nil, AuthenticationError{err}
+		if os.Getenv("CONVOX_WEB_U2F_DISABLE") != "true" {
+			browserToken := base64.StdEncoding.EncodeToString(dres)
+			endpoint, err := c.Endpoint()
+			if err != nil {
+				return nil, AuthenticationError{err}
+			}
+
+			target := url.URL{
+				Scheme: endpoint.Scheme,
+				Host:   endpoint.Host,
+				Path:   "/login/u2f",
+			}
+
+			// listen to callback url
+			dataChan := make(chan []byte)
+			tc := time.NewTicker(5 * time.Minute)
+			addr, srv, err := c.u2fCallbackServer(dataChan)
+			if err != nil {
+				return nil, AuthenticationError{err}
+			}
+
+			qParam := target.Query()
+			qParam.Add("token", browserToken)
+			qParam.Add("callback_url", addr)
+			target.RawQuery = qParam.Encode()
+			c.handler.Writef("\nOpen this link in your browser to complete the authentication:\n%s \n", target.String())
+
+			common.OpenBrowser(target.String())
+
+			defer srv.Close()
+			select {
+			case <-tc.C:
+				return nil, AuthenticationError{fmt.Errorf("callback server timeout")}
+			case body = <-dataChan:
+				c.handler.Writef("<ok>Done</ok>\n")
+			}
+		} else {
+
+			//usb yubikey
+			data, err := token.Authenticate(dres)
+			if err != nil {
+				return nil, AuthenticationError{err}
+			}
+			body = data
+			c.handler.Writef("<ok>Done</ok>\n")
 		}
 
-		c.handler.Writef("<ok>OK</ok>\n")
-
-		body = data
 		headers["Challenge"] = ares.Header.Get("Challenge")
 	}
 
@@ -91,4 +138,43 @@ func (c *Client) authenticator(cl *stdsdk.Client, res *http.Response) (http.Head
 func (c *Client) session(cl *sdk.Client) string {
 	sid, _ := c.handler.SettingReadKey("session", cl.Client.Endpoint.Host)
 	return sid
+}
+
+func (c *Client) u2fCallbackServer(dataChan chan []byte) (string, *http.Server, error) {
+	// Listen on any available port with ":0"
+	listener, err := net.Listen("tcp", ":0")
+	if err != nil {
+		return "", nil, fmt.Errorf("failed to listen on port: %v", err)
+	}
+
+	addr := fmt.Sprintf("http://localhost:%d", listener.Addr().(*net.TCPAddr).Port)
+
+	srv := &http.Server{}
+	srv.Handler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		data := r.URL.Query().Get("data")
+		if data == "" {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("data is missing"))
+			return
+		}
+
+		dataBytes, err := base64.StdEncoding.DecodeString(data)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte(fmt.Sprintf("malformed data: %s", err)))
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("you may now close the window"))
+		dataChan <- dataBytes
+	})
+
+	go func() {
+		if err := srv.Serve(listener); err != nil && !strings.Contains(err.Error(), "http: Server closed") {
+			log.Fatalf("failed to start u2f callback server: %s", err)
+		}
+	}()
+
+	return addr, srv, nil
 }
