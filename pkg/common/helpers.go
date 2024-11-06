bedb75f2cf3d5f6306134b4b87c4d4113b68a975
Md. Nure Alam Nahid <knightnahid@gmail.com>
2024-11-06 18:49:43 +0000

    Update u2f token login to use browser (#834)

--- pkg/common/helpers.go
+++ pkg/common/helpers.go
@@ -1,1 +1,24 @@
 package common
+
+import (
+	"fmt"
+	"os/exec"
+	"runtime"
+)
+
+func OpenBrowser(url string) error {
+	var err error
+
+	switch runtime.GOOS {
+	case "linux":
+		err = exec.Command("xdg-open", url).Start()
+	case "windows":
+		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+	case "darwin": // macOS
+		err = exec.Command("open", url).Start()
+	default:
+		err = fmt.Errorf("unsupported platform")
+	}
+
+	return err
+}
