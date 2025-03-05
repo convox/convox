d3c18a21887c2bb9990d84ba7bc4748b4ddcd805
nightfury1204 <knightnahid@gmail.com>
2025-03-05 15:29:12 +0000

    Downgrade k8s terraform version to version 2.11.0

--- terraform/cluster/aws/versions.tf
+++ terraform/cluster/aws/versions.tf
@@ -6,7 +6,7 @@ terraform {
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.19.0"
+      version = "~> 2.11.0"
     }
     local = {
       source  = "hashicorp/local"
--- terraform/system/do/versions.tf
+++ terraform/system/do/versions.tf
@@ -11,7 +11,7 @@ terraform {
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.19.0"
+      version = "~> 2.11.0"
     }
   }
 }
