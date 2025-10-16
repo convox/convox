7d348ca8fd8f5e7ac5bb4a8dd9b9ecc3af5b16a0
Nathan Broadbent <git@ndbroadbent.com>
2025-10-16 19:43:17 +1300

    Fixed DeepSource warnings about legacy dot index syntax (TF-L0038)

--- terraform/metrics/k8s/outputs.tf
+++ terraform/metrics/k8s/outputs.tf
@@ -1,3 +1,3 @@
 output "metrics_scraper_host" {
-  value = "http://${kubernetes_service.metrics_scraper.metadata.0.name}.${kubernetes_service.metrics_scraper.metadata.0.namespace}.svc.cluster.local:8000"
+  value = "http://${kubernetes_service.metrics_scraper.metadata[0].name}.${kubernetes_service.metrics_scraper.metadata[0].namespace}.svc.cluster.local:8000"
 }
